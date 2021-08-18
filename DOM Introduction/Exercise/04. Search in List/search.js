66dccd85827cb3979f760b222d7a6a80d622aee3
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 16:35:48 +0200

    finished Task Search in list

--- DOM Introduction/Exercise/04. Search in List/search.js
+++ DOM Introduction/Exercise/04. Search in List/search.js
@@ -1,3 +1,28 @@
 function search() {
    // TODO
+   const search = document.getElementById("searchText").value;
+
+
+   const towns = document.getElementById("towns").children;
+   let matches = 0;
+
+   for (let i = 0; i < towns.length; i++) {
+      if (towns[i].innerHTML.includes(search)) {
+         towns[i].style.textDecoration = "underline";
+         towns[i].style.fontWeight = "bold";
+         matches++;
+      }
+   }
+
+   //works but not in judge
+   // for (const i of towns) {
+   //    if (i.innerHTML.includes(search)) {
+   //       i.style.textDecoration = "underline";
+   //       i.style.fontWeight = "bold";
+   //       matches++;
+   //    }
+   // }
+
+   document.getElementById("result").textContent = `${matches} matches found`;
+
 }
