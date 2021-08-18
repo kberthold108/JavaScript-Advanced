d55f172a23746269d009c1a412032c4ed89a4c8c
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 15:37:05 +0200

    finished Task Accordion

--- DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,29 +1,28 @@
 function solve() {
   //TODO...
-    let text = document.getElementById("text").value;
-    const namingCase = document.getElementById("naming-convention").value;
+  let text = document.getElementById("text").value;
+  const namingCase = document.getElementById("naming-convention").value;
 
-    text = text.toLowerCase().split(" ");
-    if (namingCase === "Camel Case") {
-      for (let i = 1; i < text.length; i++) {
-          let word = text[i];
-          word = word.charAt(0).toUpperCase() + word.slice(1);
-          text[i] = word;
-      }
-      text = text.join("");
-      document.getElementById("result").textContent = text;
-    }else if (namingCase === "Pascal Case") {
-      for (let i = 0; i < text.length; i++) {
-          let word = text[i];
-          word = word.charAt(0).toUpperCase() + word.slice(1);
-          text[i] = word;
-      }
-      text = text.join("");
-      document.getElementById("result").textContent = text;
-    } else {
-      
-      document.getElementById("result").textContent = "Error!";
+  text = text.toLowerCase().split(" ");
+  if (namingCase === "Camel Case") {
+    for (let i = 1; i < text.length; i++) {
+      let word = text[i];
+      word = word.charAt(0).toUpperCase() + word.slice(1);
+      text[i] = word;
     }
+    text = text.join("");
+    document.getElementById("result").textContent = text;
+  } else if (namingCase === "Pascal Case") {
+    for (let i = 0; i < text.length; i++) {
+      let word = text[i];
+      word = word.charAt(0).toUpperCase() + word.slice(1);
+      text[i] = word;
+    }
+    text = text.join("");
+    document.getElementById("result").textContent = text;
+  } else {
+    document.getElementById("result").textContent = "Error!";
+  }
 
 
 }
--- DOM Introduction/Exercise/03. Accordion/accordion.js
+++ DOM Introduction/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+
+    if (document.getElementsByClassName("button")[0].textContent === "More") {
+        document.getElementById("extra").style.display = "block";
+        document.getElementsByClassName("button")[0].textContent = "Less";
+    } else if (document.getElementsByClassName("button")[0].textContent === "Less") {
+        document.getElementById("extra").style.display = "none";
+        document.getElementsByClassName("button")[0].textContent = "More";
+    }
+
 }
