58d452e47eb32703233ecec9b64f23cf269d9962
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 12:02:46 +0200

    finished Edit Element

--- DOM Introduction/Lab/01.Edit Element/EditElement.js
+++ DOM Introduction/Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+    const elementText = element.textContent;
+    const replacedText = elementText.replace(match, replacer); 
+    element.textContent = replacedText;
 }
