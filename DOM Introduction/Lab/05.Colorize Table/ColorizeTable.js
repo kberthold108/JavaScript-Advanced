b3e6c1435267c63e258c1546ba53e70374e52338
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 13:20:58 +0200

    finished task Colorized Table

--- DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
+++ DOM Introduction/Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,9 @@
 function colorize() {
-    // TODO
+    const tbody = document.getElementsByTagName("tbody")[0];
+    const children = Array.from(tbody.children);
+    for (let i = 1; i < children.length; i++) {
+        if (i % 2 !== 0) {
+            children[i].style.background = "Teal";
+        }
+    }
 }
