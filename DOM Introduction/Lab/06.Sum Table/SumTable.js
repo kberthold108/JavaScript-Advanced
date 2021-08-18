e84d3597aae8dd861f29edd7c1f1cd04ca675831
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 13:33:15 +0200

    finished Task Sum Table

--- DOM Introduction/Lab/06.Sum Table/SumTable.js
+++ DOM Introduction/Lab/06.Sum Table/SumTable.js
@@ -1,3 +1,11 @@
 function sumTable() {
-
+    let sum = 0;
+    const resultElement = document.getElementsById("sum");
+    const tbody = document.querySelector("tbody");
+    const rows = Array.from(tbody.children);
+    for (let i = 1; i < rows.length - 1; i++) {
+        const children = rows[i].children;
+        sum += Number(children[1].textContent);
+    }
+    resultElement.textContent = sum;
 }
