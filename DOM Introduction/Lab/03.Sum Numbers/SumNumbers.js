bbc38c5f2c0901fc0221b975fb8960f45992d749
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 12:56:02 +0200

    finished task Show More

--- DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
+++ DOM Introduction/Lab/03.Sum Numbers/SumNumbers.js
@@ -1,9 +1,6 @@
 function calc() {
     const num1  = Number(document.getElementById("num1").value);
     const num2  = Number(document.getElementById("num2").value);
-
-    const sum = num1 + +num2;
+    const sum = num1 + num2;
     document.getElementById("sum").value = sum;
-    
-
 }
