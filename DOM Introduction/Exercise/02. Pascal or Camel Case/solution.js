bd41fea84c33d1a5a0e234c795a6a574a6815782
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 15:22:59 +0200

    finished Task Pascal or Camel Case

--- DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
+++ DOM Introduction/Exercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,29 @@
 function solve() {
   //TODO...
+    let text = document.getElementById("text").value;
+    const namingCase = document.getElementById("naming-convention").value;
+
+    text = text.toLowerCase().split(" ");
+    if (namingCase === "Camel Case") {
+      for (let i = 1; i < text.length; i++) {
+          let word = text[i];
+          word = word.charAt(0).toUpperCase() + word.slice(1);
+          text[i] = word;
+      }
+      text = text.join("");
+      document.getElementById("result").textContent = text;
+    }else if (namingCase === "Pascal Case") {
+      for (let i = 0; i < text.length; i++) {
+          let word = text[i];
+          word = word.charAt(0).toUpperCase() + word.slice(1);
+          text[i] = word;
+      }
+      text = text.join("");
+      document.getElementById("result").textContent = text;
+    } else {
+      
+      document.getElementById("result").textContent = "Error!";
+    }
+
+
 }
