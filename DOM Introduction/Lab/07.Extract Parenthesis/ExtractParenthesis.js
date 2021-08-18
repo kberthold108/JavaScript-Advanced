8e58c9ff44912c26406968630ab59c87197e096b
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-18 13:45:52 +0200

    finished task Extract Parenthesis

--- DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ DOM Introduction/Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,7 @@
-function extract(content) {
+function extract(id) {
+    const regex = /\((?<text>.+?)\)/gm;
+    const text = document.getElementById(id).textContent;
+    const matches = Array.from(text.matchAll(regex)).map(match => match.groups.text).join("; ");
 
+    return matches;
 }
