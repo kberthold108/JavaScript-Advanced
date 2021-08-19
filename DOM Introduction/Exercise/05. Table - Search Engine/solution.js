a9a8550e9a009745be77c5e577d2f20165a95715
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-19 10:19:21 +0200

    finished Task Table - Search Engine

--- DOM Introduction/Exercise/05. Table - Search Engine/solution.js
+++ DOM Introduction/Exercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,19 @@
 function solve() {
-   document.querySelector('#searchBtn').addEventListener('click', onClick);
+   document.querySelector("#searchBtn").addEventListener("click", onClick);
 
    function onClick() {
-      //   TODO:
-
+      let search = document.getElementById("searchField").value;
+      const students = document.getElementsByTagName("tr");
+      if (search === "") {
+         return "";
+      }
+      for (let i = 2; i < students.length; i++) {
+         students[i].classList.remove("select");
+      }
+      for (let i = 2; i < students.length; i++) {
+         if (students[i].textContent.includes(search)) {
+            students[i].classList.add("select");
+         }
+      }
    }
 }
