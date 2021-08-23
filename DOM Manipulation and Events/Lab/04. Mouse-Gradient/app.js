12d0ec5820bbb859975dd76eebc3b20690f9fd6e
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-23 16:12:44 +0200

    finished Task Mouse Gradient

--- DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
+++ DOM Manipulation and Events/Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,12 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+
+    const gradiantBar =  document.getElementById("gradient");
+    const result = document.getElementById("result");
+
+    function updateDisplay(event) {
+        result.innerText = Math.floor((event.offsetX / event.target.clientWidth) * 100) + "%";
+
+      }
+    gradiantBar.addEventListener("mousemove", updateDisplay, false);
+
 }
