30182a05dbef60063a6d7f84fb499f18c7478d1d
Kenneth Berthold <kenneth.berthold@quajoo.com>
2021-08-23 12:56:29 +0200

    finished Task Number Convertor

--- DOM Introduction/Exercise/09. Number Convertor/solution.js
+++ DOM Introduction/Exercise/09. Number Convertor/solution.js
@@ -1,4 +1,28 @@
 function solve() {
+    const convertoption = document.getElementById("selectMenuTo");
+    const [button] = document.getElementsByTagName("button");
 
-    //TODO...
+    const option1 = document.createElement("option");
+    const option2 = document.createElement("option");
+
+    option1.value = "binary";
+    option1.text = "Binary";
+
+    option2.value = "hexadecimal";
+    option2.text = "Hexadecimal";
+
+    convertoption.add(option1, null);
+    convertoption.add(option2, null);
+
+    button.addEventListener("click", onClick);
+
+    function onClick() {
+        const number = Number(document.getElementById("input").value);
+        const opation = document.getElementById("selectMenuTo").value;
+        if (opation === "hexadecimal") {
+            document.getElementById("result").value = number.toString(16).toUpperCase();
+        } else {
+            document.getElementById("result").value = number.toString(2);
+        }
+    }
 }
